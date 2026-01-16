8e0c71d793703462cb742233db9b5daded27eaa8
k0278kim <k0278kim@gmail.com>
2026-01-16 14:24:41 +0900

    fix: 테스트 스크립트에서 `checkpoint.pth` 대신 `checkpoint_best.pth`를 사용하도록 수정

--- script/test.sh
+++ script/test.sh
@@ -2,7 +2,7 @@ cd ..
 
 wandb disabled
 
-for ckpt in ./save/rn50_50ep/checkpoint.pth; do
+for ckpt in ./save/rn50_50ep/checkpoint_best.pth; do
 
     echo $ckpt
     python -m torch.distributed.run --nproc_per_node=1 classification/train.py \
